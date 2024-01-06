76f17ed84830408768eeabb2ded1d466f80a2c47
pedrohassen <pedro_hasse@yahoo.com.br>
2024-01-05 21:25:19 -0300

    add app.js, with connection to routes

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const router = require('./routes');
 
 const app = express();
-app.use(cors());
 app.use(express.json());
-
-app.get('/', (_req, res) => {
-  res.send('Hello World!');
-});
+app.use(cors());
+app.use(router);
 
 module.exports = app;
