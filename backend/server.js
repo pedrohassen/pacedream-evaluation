2ea95c686811847a4dbe3f9fc992dfa63658be02
pedrohassen <pedro_hasse@yahoo.com.br>
2024-01-07 11:01:26 -0300

    refactor: 💡 prisma for mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,17 @@
 const dotenv = require('dotenv').config(); // eslint-disable-line
+const mongoose = require('mongoose');
 const app = require('./app');
+
+mongoose.connect('mongodb://mongodb:27017/pacedream', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
 
 const server = app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
